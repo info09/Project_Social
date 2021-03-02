e5e45eecd26cb98f60d66a6b5a634f727ac6cb3d
HuyTQ <huytq3103@gmail.com>
2021-03-02 17:07:48 +0700

    connect database using mongoose

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import { Route } from 'core/interfaces';
 import express from 'express';
+import mongoose from 'mongoose';
 
 class App {
   public app: express.Application;
@@ -10,6 +11,7 @@ class App {
     this.port = process.env.PORT || 5000;
 
     this.initializeRoutes(routes);
+    this.connectToDatabase();
   }
 
   public listen() {
@@ -22,6 +24,22 @@ class App {
     routes.forEach((route) => {
       this.app.use('/', route.router);
     });
+  }
+
+  private connectToDatabase(){
+    const connectString = 'mongodb+srv://huytq_sa:ZNpmNKrUyUULFxVG@master.uwiff.mongodb.net/project_social?retryWrites=true&w=majority';
+    try {
+      mongoose.connect(connectString, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: true,
+        useCreateIndex: true
+      });
+
+      console.log('Database connected...');
+    } catch (error) {
+      console.log('Database connect failed');
+    }
   }
 }
 
