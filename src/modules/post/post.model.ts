db298aed683b97b1c9c060aff309b2abe5e4ec51
HuyTQ <huytq3103@gmail.com>
2021-05-10 22:21:13 +0700

    tạo model cho bài viết

---
+++ src/modules/post/post.model.ts
@@ -0,0 +1,52 @@
+import mongoose, { Document } from "mongoose";
+import { IPost } from "./post.interface";
+const PostSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+  },
+  text: {
+    type: String,
+    require: true,
+  },
+  name: {
+    type: String,
+  },
+  avatar: {
+    type: String,
+  },
+  likes: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+      },
+    },
+  ],
+  comments: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+      },
+      text: {
+        type: String,
+        require: true,
+      },
+      name: {
+        type: String,
+      },
+      avatar: {
+        type: String,
+      },
+      date: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export default mongoose.model<IPost & Document>("post", PostSchema);
